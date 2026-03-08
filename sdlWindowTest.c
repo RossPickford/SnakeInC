8105b9c9187a968088c3ac035cac06e98c449370
Ross Pickford <ross.pickford34@gmail.com>
2026-03-08 17:14:22 +0000

    added sdl3 repo, and created simple project 'hello.c'

--- sdlWindowTest.c
+++ sdlWindowTest.c
@@ -1,29 +1,12 @@
 #include <stdio.h>
-#include <sdl2/SDL.h>
+#include "SDL.h"
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 int main(void)
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window* win = SDL_CreateWindow("my window", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-
-    if (!win)
-    {
-        printf("Failed to create window: %s", SDL_GetError());
-        return 1;
-    }
-
-    SDL_Surface* winSurface = SDL_GetWindowSurface(win);
-
-    SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 255, 90, 120));
-
-    SDL_UpdateWindowSurface(win);
-
-    system("pause");
-
-    SDL_DestroyWindow(win);
-
-    SDL_Quit();
 
     return 0;
     
