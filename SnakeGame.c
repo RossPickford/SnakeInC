c8882b3682701ddcf7f3ffd3982364d0aec50a1d
Ross Pickford <ross.pickford34@gmail.com>
2026-03-14 03:05:48 +0000

    made basic controllable snake, just need fine tune a bit more

--- SnakeGame.c
+++ SnakeGame.c
@@ -1,3 +1,9 @@
+typedef struct
+{
+    int x;
+    int y;
+} Int_Vector2;
+
 #define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
@@ -9,7 +15,13 @@ static SDL_Renderer *renderer = NULL;
 
 static SDL_FRect wallBackground;
 static SDL_FRect mainBackground;
-static SDL_FRect* snake;
+static SDL_FRect *snake;
+
+static Int_Vector2 dir;
+
+static Uint64 previousTick = 0, tickDelta = 0;
+
+#define CLOCK_SPEED 250
 
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 640
@@ -17,7 +29,7 @@ static SDL_FRect* snake;
 
 #define GREY 30, 30, 30
 
-void setCoords(SDL_FRect *rect, Uint64 tickDelta);
+void moveSnake(SDL_FRect *head, Uint64 length);
 void renderWalls();
 void drawSnake();
 void renderFood();
@@ -45,8 +57,18 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     mainBackground.h = WINDOW_HEIGHT - (2 * GRID_WIDTH);
     mainBackground.w = WINDOW_WIDTH - (2 * GRID_WIDTH);
     mainBackground.x = mainBackground.y = GRID_WIDTH;
-    
-    snake = (SDL_FRect*)malloc(sizeof(SDL_FRect) * (powf((WINDOW_HEIGHT - (GRID_WIDTH * 2) / (float) GRID_WIDTH), 2)));
+
+    snake = (SDL_FRect *)malloc(sizeof(SDL_FRect) * (powf((WINDOW_HEIGHT - (GRID_WIDTH * 2) / (float)GRID_WIDTH), 2)));
+
+    for (int i = 0; i < 3; i++)
+    {
+        (snake + i)->h = (snake + i)->w = GRID_WIDTH;
+        (snake + i)->y = (mainBackground.h - GRID_WIDTH) / 2.0f;
+        (snake + i)->x = (snake + i)->y + (GRID_WIDTH * i);
+    }
+
+    dir.x = -1.0f;
+    dir.y = 0.0f;
 
     return SDL_APP_CONTINUE;
 }
@@ -56,20 +78,54 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
     if (event->type == SDL_EVENT_QUIT)
         return SDL_APP_SUCCESS;
 
+    if (event->type == SDL_EVENT_KEY_DOWN)
+    {
+        if (event->key.key == SDLK_LEFT && dir.x != 1)
+        {
+            dir.x = -1;
+            dir.y = 0;
+        }
+        else if (event->key.key == SDLK_RIGHT && dir.x != -1)
+        {
+            dir.x = 1;
+            dir.y = 0;
+        }
+        else if (event->key.key == SDLK_UP && dir.y != 1)
+        {
+            dir.x = 0;
+            dir.y = -1;
+        }
+        else if (event->key.key == SDLK_DOWN && dir.y != -1)
+        {
+            dir.x = 0;
+            dir.y = 1;
+        }
+    }
+
     return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
+    Uint64 currentTick = SDL_GetTicks();
+
+    tickDelta += (currentTick - previousTick);
+
+    if (tickDelta >= CLOCK_SPEED)
+    {
+        moveSnake(snake, 3);
+        tickDelta -= CLOCK_SPEED;
+    }
+
+    previousTick = currentTick;
+
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
 
     renderWalls();
-    // drawSnake();
+    drawSnake();
 
     SDL_RenderPresent(renderer);
-
-    // previousTick = now;
 
     return SDL_APP_CONTINUE;
 }
@@ -79,8 +135,16 @@ void SDL_AppQuit(void *appstate, SDL_AppResult result)
     free(snake);
 }
 
-void setCoords(SDL_FRect *rect, Uint64 tickDelta)
+void moveSnake(SDL_FRect *head, Uint64 length)
 {
+    for (int i = length - 1; i >= 1; i--)
+    {
+        (head + i)->x = (head + i - 1)->x;
+        (head + i)->y = (head + i - 1)->y;
+    }
+
+    head->x += (GRID_WIDTH * dir.x);
+    head->y += (GRID_WIDTH * dir.y);
 }
 
 void renderWalls()
@@ -90,4 +154,16 @@ void renderWalls()
 
     SDL_SetRenderDrawColor(renderer, GREY, SDL_ALPHA_OPAQUE);
     SDL_RenderFillRect(renderer, &mainBackground);
+}
+
+void drawSnake()
+{
+    SDL_SetRenderDrawColor(renderer, 15, 210, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderFillRect(renderer, snake);
+    SDL_SetRenderDrawColor(renderer, 15, 110, 0, SDL_ALPHA_OPAQUE);
+
+    for (int i = 1; i < 3; i++)
+    {
+        SDL_RenderFillRect(renderer, (snake + i));
+    }
 }
