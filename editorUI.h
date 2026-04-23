e6e243f50135fb267eb6ab9cdac6b1990959a658
VND Pickford <Pickford.Ross@bcg.com>
2026-04-23 16:46:49 +0100

    input text box functionality created, need to implement as feature

--- editorUI.h
+++ editorUI.h
@@ -1,6 +1,8 @@
 #include <SDL3/SDL.h>
 #include <SDL3_ttf/SDL_ttf.h>
 
+#define MAX_CHAR_INPUT 20
+
 typedef enum
 {
     PRESSED,
@@ -30,6 +32,20 @@ typedef struct
     E_ButtonState previousState;
 } ButtonData;
 
+typedef struct
+{
+    char *input;
+    int maxInputLength;
+    int currentPos;
+    TTF_Font *font;
+    float fontSize;
+    SDL_FRect textBox;
+    SDL_FRect cursor;
+    SDL_Color *colour;
+    bool selected;
+
+} InputTextBoxData;
+
 bool UpdateText(TextData *textData, SDL_Renderer *renderer)
 {
     if (textData->text == NULL || textData->font == NULL)
@@ -53,8 +69,19 @@ bool UpdateText(TextData *textData, SDL_Renderer *renderer)
     if (textData->texture != NULL)
         SDL_DestroyTexture(textData->texture);
 
-    textData->texture = SDL_CreateTextureFromSurface(renderer, textSurface);
+    if ((textData->texture = SDL_CreateTextureFromSurface(renderer, textSurface)) == NULL)
+    {
+        SDL_Log("Failed to create texture: %s", SDL_GetError());
+        return false;
+    }
+
     SDL_DestroySurface(textSurface);
+
+    if (!SDL_GetTextureSize(textData->texture, &textData->rect.w, &textData->rect.h))
+    {
+        SDL_Log("Failed to get texture size: %s", SDL_GetError());
+        return false;
+    }
 
     return true;
 }
@@ -84,6 +111,13 @@ SDL_Color GetButtonTextColour(ButtonData *btnData)
     }
 }
 
+/**
+ * This checks and will update the current stat of a button based on the given mouse position.
+ *
+ * This will return true if the button has changed state and false if the state has remained the same.
+ *
+ * Use this in an if statement to add in logic changes if the change of state was true
+ */
 bool CheckButtonState(ButtonData *btnData, SDL_Event *event, SDL_FPoint *mousePos)
 {
     E_ButtonState btnState = NONE;
@@ -102,7 +136,6 @@ bool CheckButtonState(ButtonData *btnData, SDL_Event *event, SDL_FPoint *mousePo
     if (btnState != NONE && ChangeButtonState(btnData, btnState))
     {
         btnData->textData.colour = GetButtonTextColour(btnData);
-
         return true;
     }
 
@@ -115,13 +148,12 @@ bool CheckButtonState(ButtonData *btnData, SDL_Event *event, SDL_FPoint *mousePo
  * This DOES NOT assign the pointer texture - call UpdateText() after calling this function to create
  * and assign a texture and it's corresponding pointer to the text data struct.
  */
-void AssignTextData(TextData *txtData, char *txt, TTF_Font *font, float fontSize, SDL_Color colour /*, SDL_FRect rect*/)
+void AssignTextData(TextData *txtData, char *txt, TTF_Font *font, float fontSize, SDL_Color colour)
 {
     txtData->text = txt;
     txtData->font = font;
     txtData->fontSize = fontSize;
     txtData->colour = colour;
-    // txtData->rect = rect;
 }
 
 /**
@@ -136,4 +168,100 @@ void AssignButtonData(ButtonData *btn, SDL_Color display, SDL_Color highlight, S
     btn->selectColour = select;
     btn->currentState = curState;
     btn->previousState = prevState;
+}
+
+bool SetTextPosition(TextData *txtData, float x, float y)
+{
+    txtData->rect.x = x;
+    txtData->rect.y = y;
+
+    return true;
+}
+
+bool InitInputTextBox(InputTextBoxData *txtBx, int maxInputLength, TTF_Font *font, float fontSize, SDL_Color *colour)
+{
+    txtBx->maxInputLength = maxInputLength;
+    txtBx->font = font;
+    txtBx->fontSize = fontSize;
+    txtBx->colour = colour;
+    txtBx->input = (char *)SDL_malloc(sizeof(char) * maxInputLength);
+    *txtBx->input = '\0';
+    txtBx->currentPos = 0;
+}
+
+bool RenderInputTextBox(InputTextBoxData *txtBx, SDL_Renderer *renderer)
+{
+    SDL_Surface *surf = TTF_RenderText_Solid(txtBx->font, txtBx->input, sizeof(char) * txtBx->maxInputLength, *(txtBx->colour));
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surf);
+    SDL_DestroySurface(surf);
+
+    SDL_GetTextureSize(texture, &txtBx->textBox.w, &txtBx->textBox.h);
+    SDL_DestroyTexture(texture);
+
+    SDL_SetRenderDrawColor(renderer, txtBx->colour->r, txtBx->colour->g, txtBx->colour->b, txtBx->colour->a);
+    SDL_RenderRect(renderer, &txtBx->textBox);
+
+    SDL_Rect viewPort;
+
+    viewPort.x = txtBx->textBox.x;
+    viewPort.y = txtBx->textBox.y;
+    viewPort.w = txtBx->textBox.w;
+    viewPort.h = txtBx->textBox.h;
+
+    SDL_SetRenderViewport(renderer, &viewPort);
+
+    SDL_Surface *displaytextSurf = TTF_RenderText_Solid(txtBx->font, txtBx->input, txtBx->currentPos, *txtBx->colour);
+    SDL_Texture *displayTexture = SDL_CreateTextureFromSurface(renderer, displaytextSurf);
+    SDL_DestroySurface(displaytextSurf);
+
+    SDL_FRect rect;
+    rect.x = rect.y = 0.0f;
+    SDL_GetTextureSize(displayTexture, &rect.w, &rect.h);
+    SDL_RenderTexture(renderer, displayTexture, NULL, &rect);
+    SDL_DestroyTexture(displayTexture);
+
+    surf = TTF_RenderText_Solid(txtBx->font, " ", 1, *txtBx->colour);
+    texture = SDL_CreateTextureFromSurface(renderer, surf);
+    SDL_DestroySurface(surf);
+
+    SDL_GetTextureSize(texture, &txtBx->cursor.w, &txtBx->cursor.h);
+    SDL_DestroyTexture(texture);
+
+    txtBx->cursor.x = rect.w;
+    txtBx->cursor.y = rect.y;
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &txtBx->cursor);
+
+    SDL_SetRenderViewport(renderer, NULL);
+
+    return true;
+}
+
+bool InsertCharacter(InputTextBoxData *txtBx, const char *input)
+{
+    if (txtBx->currentPos >= txtBx->maxInputLength)
+        return false;
+
+    int len = (int)SDL_strlen(input);
+    char *copy = (char *)SDL_malloc(len);
+
+    SDL_strlcpy(copy, input, (len + 1));
+
+    if (SDL_strcmp(copy, "Space") == 0)
+    {
+        *copy = ' ';
+        *(copy + 1) = '\0';
+    }
+
+    if (*(copy + 1) != '\0')
+    {
+        SDL_free(copy);
+        return false;
+    }
+
+    *(txtBx->input + txtBx->currentPos++) = *copy;
+    SDL_free(copy);
+
+    return true;
 }
--- editor_ui.c
+++ editor_ui.c
@@ -4,6 +4,11 @@
 #define APP_CONTINUE true
 #define APP_END false
 
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
+
+SDL_DialogFileCallback fileCallback;
+
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 
@@ -20,6 +25,10 @@ char *createLayoutTxt = "Create Layout";
 
 ButtonData loadLayoutBtn;
 char *loadLayoutTxt = "Load Layout...";
+
+InputTextBoxData createLayoutInput;
+
+SDL_FPoint mousePos;
 
 bool initSystems()
 {
@@ -48,8 +57,14 @@ bool InitAllText()
     AssignTextData(&createLayoutBtn.textData, createLayoutTxt, font, 20.0f, createLayoutBtn.displayColour);
     AssignTextData(&loadLayoutBtn.textData, loadLayoutTxt, font, 20.0f, loadLayoutBtn.displayColour);
 
+    InitInputTextBox(&createLayoutInput, MAX_CHAR_INPUT, font, 20.0f, &white);
+    createLayoutInput.textBox.x = WINDOW_WIDTH / 3.0f;
+    createLayoutInput.textBox.y = WINDOW_HEIGHT / 3.0f;
+
     return UpdateText(&createLayoutBtn.textData, renderer) &&
-           UpdateText(&loadLayoutBtn.textData, renderer);
+           UpdateText(&loadLayoutBtn.textData, renderer) &&
+           SetTextPosition(&createLayoutBtn.textData, 0.0f, 0.0f) &&
+           SetTextPosition(&loadLayoutBtn.textData, 0.0f, (createLayoutBtn.textData.rect.h * 1.5f));
 }
 
 bool AppInit()
@@ -57,18 +72,57 @@ bool AppInit()
     if (!initSystems())
         return APP_END;
 
-    if (!SDL_CreateWindowAndRenderer("UI Editor", 1920, 1080, SDL_WINDOW_FULLSCREEN, &window, &renderer))
+    if (!SDL_CreateWindowAndRenderer("UI Editor", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_FULLSCREEN, &window, &renderer))
     {
         SDL_Log("Failed to create window and renderer: %s", SDL_GetError());
         return APP_END;
     }
 
-    SDL_SetRenderLogicalPresentation(renderer, 1920, 1080, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+    SDL_SetRenderLogicalPresentation(renderer, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
     if (!InitAllText())
         return APP_END;
 
     return APP_CONTINUE;
+}
+
+static const SDL_DialogFileFilter filters[] = {
+    {"PNG images", "png"},
+    {"JPEG images", "jpg;jpeg"},
+    {"All images", "png;jpg;jpeg"},
+    {"All files", "*"}};
+
+static void SDLCALL callback(void *userdata, const char *const *filelist, int filter)
+{
+    if (!filelist)
+    {
+        SDL_Log("An error occured: %s", SDL_GetError());
+        return;
+    }
+    else if (!*filelist)
+    {
+        SDL_Log("The user did not select any file.");
+        SDL_Log("Most likely, the dialog was canceled.");
+        return;
+    }
+
+    while (*filelist)
+    {
+        SDL_Log("Full path to selected file: '%s'", *filelist);
+        filelist++;
+    }
+
+    if (filter < 0)
+    {
+        SDL_Log("The current platform does not support fetching "
+                "the selected filter, or the user did not select"
+                " any filter.");
+    }
+    else if (filter < SDL_arraysize(filters))
+    {
+        SDL_Log("The filter selected by the user is '%s' (%s).",
+                filters[filter].pattern, filters[filter].name);
+    }
 }
 
 bool AppEvent()
@@ -79,7 +133,20 @@ bool AppEvent()
         if (event.type == SDL_EVENT_KEY_DOWN && event.key.scancode == SDL_SCANCODE_ESCAPE)
             return APP_END;
 
-        
+        if (event.type == SDL_EVENT_KEY_DOWN)
+            InsertCharacter(&createLayoutInput, SDL_GetKeyName(event.key.key));
+
+        SDL_ConvertEventToRenderCoordinates(renderer, &event);
+        mousePos.x = event.button.x;
+        mousePos.y = event.button.y;
+
+        if (CheckButtonState(&createLayoutBtn, &event, &mousePos))
+            ;
+        if (CheckButtonState(&loadLayoutBtn, &event, &mousePos) && loadLayoutBtn.currentState == RELEASED)
+        {
+            SDL_Log("clicked on");
+            SDL_ShowOpenFileDialog(callback, NULL, window, NULL, 0, NULL, false);
+        }
     }
 
     return APP_CONTINUE;
@@ -87,20 +154,28 @@ bool AppEvent()
 
 bool AppIterate()
 {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_SetRenderDrawColor(renderer, 20, 20, 20, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
 
     SDL_FRect rect;
-    rect.x = 0.0f;
-    rect.y = 0.0f;
 
-    SDL_GetTextureSize(createLayoutBtn.textData.texture, &rect.w, &rect.h);
-    SDL_RenderTexture(renderer, createLayoutBtn.textData.texture, NULL, &rect);
+    rect.x = rect.y = 0.0f;
+    rect.w = WINDOW_WIDTH;
+    rect.h = WINDOW_HEIGHT;
 
-    rect.y += (rect.h * 1.5f);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderFillRect(renderer, &rect);
 
-    SDL_GetTextureSize(loadLayoutBtn.textData.texture, &rect.w, &rect.h);
-    SDL_RenderTexture(renderer, loadLayoutBtn.textData.texture, NULL, &rect);
+    RenderInputTextBox(&createLayoutInput, renderer);
+
+    if (createLayoutBtn.currentState != createLayoutBtn.previousState)
+        UpdateText(&createLayoutBtn.textData, renderer);
+
+    if (loadLayoutBtn.currentState != loadLayoutBtn.previousState)
+        UpdateText(&loadLayoutBtn.textData, renderer);
+
+    SDL_RenderTexture(renderer, createLayoutBtn.textData.texture, NULL, &createLayoutBtn.textData.rect);
+    SDL_RenderTexture(renderer, loadLayoutBtn.textData.texture, NULL, &loadLayoutBtn.textData.rect);
 
     SDL_RenderPresent(renderer);
 
